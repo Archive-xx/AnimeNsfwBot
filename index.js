612cffb70680fd28a1e2de9758ac790d5e9e61b5
David <32846768+31ank@users.noreply.github.com>
2019-03-24 00:17:41 +0100

    Improved reddit posts on specific times

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 const config = require("./botconfig.json");
-const nekoclient = require('nekos.life');
 const Discord = require("discord.js");
-const RichEmbed = require("discord.js");
-const neko = new nekoclient();
 const request = require("request");
 // need rework https://nekos.life/api/v2/endpoints
 const nsfwlist = ["nsfw_neko_gif", "randomHentaiGif", "pussy", "nekoGif", "neko", "lesbian", "kuni", "cumsluts", "classic", "boobs", "bJ", "anal", "avatar", "yuri", "trap", "tits", "girlSoloGif", "girlSolo", "smallBoobs", "pussyWankGif", "pussyArt", "kemonomimi", "kitsune", "keta", "holo", "holoEro", "hentai", "futanari", "femdom", "feetGif", "eroFeet", "feet", "ero", "eroKitsune", "eroKemonomimi", "eroNeko", "eroYuri", "cumArts", "blowJob", "pussyGif"];
@@ -74,31 +71,57 @@ bot.on("message", async message => {
             if(err) return console.log(err);
             console.log("Updated channels file:" + message.channel.id + ":" + args);
         });
+        return message.channel.send("Added channel to update list with subreddit: " + args);
     }
 
     if(cmd == `${prefix}remove`){
+        let channelid = message.channel.id;
+        return message.channel.send("Faild! Code: ```RCF:" + channelid + "```Please message this code to @31ank#4002")
+    }
 
+    if(cmd == `${prefix}force`){
+        sendMessage(true);
     }
 });
 
-function sendMessage(){
+function sendMessage(force = false){
     let rawdata = fs.readFileSync("./channels.json");
     let out_channels = JSON.parse(rawdata);
-    out_channels.entries.forEach(element => {
-        let channel = element["ch"];
-        try{
-            request(`https://www.reddit.com/r/${element.sub}/rising/.json?limit={1}`, function(error, response, body){
-                let items = JSON.parse(body);
-                let imgurl = items["data"]["children"]["0"]["data"]["url"];
-                bot.channels.get(channel).send(imgurl);
-            });
-        } catch {
-            console.log("Send message error\nBot offline?")
+    var times = ['00', '04', '08', '12', '16', '20'];
+    console.log(times);
+    var update = false;
+    currentDate = new Date();
+    times.forEach(function(element){
+        let time_min = new Date(currentDate.getTime());
+        time_min.setHours(element);
+        time_min.setMinutes(0);
+        let time_max = new Date(time_min.getTime());
+        time_max.setMinutes('30');
+        if(currentDate.getTime() > time_min.getTime() && currentDate.getTime() < time_max.getTime()){
+            update = true;
         }
-        setTimeout(sendMessage, 10000);
     });
+    if(force){
+        update = true;
+    }
+    if(update){
+        console.log("Updatetime: " + currentDate.getTime());
+        out_channels.entries.forEach(element => {
+            let channel = element["ch"];
+            try{
+                request(`https://www.reddit.com/r/${element.sub}/rising/.json?limit={1}`, function(error, response, body){
+                    let items = JSON.parse(body);
+                    let imgurl = items["data"]["children"]["0"]["data"]["url"];
+                    bot.channels.get(channel).send(imgurl);
+                });
+            } catch {
+                console.log("Send message error\nBot offline?")
+            }
+            setTimeout(sendMessage, 1800000); // Every half hour check
+        });
+    }
 }
 
 bot.login(config.token);
 
-setTimeout(sendMessage, 5000);
+setTimeout(sendMessage, 10000);  // Wait till bot is online
